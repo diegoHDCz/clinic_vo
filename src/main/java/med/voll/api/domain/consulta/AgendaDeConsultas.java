fb47f6f9ee34e642f13ae63ca85e3ed9a7fc5822
diego.czajka <diego.czajka@gmail.com>
2023-03-28 22:35:01 -0300

    consultas-repository-dtos

---
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,10 @@
+package med.voll.api.domain.consulta;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultas {
+    public void agendar(DadosAgendamentoConsulta dados){
+
+    }
+}
